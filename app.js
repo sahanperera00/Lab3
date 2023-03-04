4c738b0fe5b00720b3524e404ffeb6e397d18c9d
Sahan Perera <sahanvimukthi00@gmail.com>
2023-03-04 09:40:26 +0530

    create web server feature

--- app.js
+++ app.js
@@ -11,3 +11,13 @@ fs.writeFile("file.txt", "Hello World!", function (err) {
   if (err) throw err;
   console.log("File saved!");
 });
+
+// Feature: Create web server
+const http = require("http");
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("Hello World!");
+    res.end();
+  })
+  .listen(8080);

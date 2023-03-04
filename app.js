f628bb5072b32587498f097f2a406c3fe737b92b
Sahan Perera <sahanvimukthi00@gmail.com>
2023-03-04 09:34:16 +0530

    add write file feature

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
+// Feature: Read file
 const fs = require("fs");
 fs.readFile("file.txt", "utf8", function (err, data) {
   if (err) throw err;
   console.log(data);
+});
+
+// Feature: Write file
+const fs = require("fs");
+fs.writeFile("file.txt", "Hello World!", function (err) {
+  if (err) throw err;
+  console.log("File saved!");
 });

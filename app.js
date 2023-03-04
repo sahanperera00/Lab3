15320c987b05f89f806b12097324037389fbb63e
Sahan Perera <sahanvimukthi00@gmail.com>
2023-03-04 09:58:04 +0530

    create async await feature

--- app.js
+++ app.js
@@ -57,3 +57,14 @@ myPromise
   .catch((error) => {
     console.log(error);
   });
+
+// Feature: Use async/await
+async function myFunction() {
+  try {
+    const result = await myPromise;
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
+}
+myFunction();
